24ddac56bc92777521f5032c9b5c7e79e64da8b0
Thiago <thiago_placa99@outlook.com>
2026-08-25 16:44:58 -0300

    Custom Pipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
