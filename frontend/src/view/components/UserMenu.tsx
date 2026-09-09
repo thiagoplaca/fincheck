c500a59a11048990e6857f709bc9165fe2b8d831
Thiago <thiago_placa99@outlook.com>
2026-09-09 19:06:30 -0300

    Front: Dashboard Account UI

---
+++ frontend/src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export function UserMenu() {
+  return (
+    <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border-teal-100">
+      <span className="text-sm tracking-[-0.05px] font-medium text-teal-900">
+        TP
+      </span>
+    </div>
+  );
+}
